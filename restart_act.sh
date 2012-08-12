2a84bd7bdc597925ce5905c98ad09e52a51961d6
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2012-08-12 00:40:43 -0300

    removendo cache antes de reiniciar o restart_act.sh
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

--- restart_act.sh
+++ restart_act.sh
@@ -6,6 +6,9 @@ source /data/tools/perl5/etc/bashrc
 # matando processo na marreta
 /bin/fuser 8091/tcp -k
 
+# apagando cache
+rm -rf /data/sites/yapcbrasil.org.br/Act/var
+
 # iniciando act
 ACTHOME=/data/sites/yapcbrasil.org.br/Act plackup -I /data/sites/yapcbrasil.org.br/Act/lib  -s Starman --daemonize --port 8091 --host 10.0.0.6 --workers 3 --user=www-data /data/sites/yapcbrasil.org.br/Act/production.psgi
 
