55edc4c1debe0dd89f53d389a8bda9b8b73ae10b
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2012-08-12 01:00:05 -0300

    comentando error_log
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

--- restart_act.sh
+++ restart_act.sh
@@ -10,6 +10,16 @@ source /data/tools/perl5/etc/bashrc
 rm -rf /data/sites/yapcbrasil.org.br/Act/var
 
 # iniciando act
-ACTHOME=/data/sites/yapcbrasil.org.br/Act plackup -I /data/sites/yapcbrasil.org.br/Act/lib  -s Starman --daemonize --port 8091 --host 10.0.0.6 --workers 3 --user=www-data /data/sites/yapcbrasil.org.br/Act/production.psgi
 
+ACTHOME=/data/sites/yapcbrasil.org.br/Act \
+    plackup -I/data/sites/yapcbrasil.org.br/Act/lib  \
+    -s Starman \
+    --daemonize \
+    --port 8091 \
+    --host 10.0.0.6 \
+    --workers 3 \
+    --user=www-data \
+    /data/sites/yapcbrasil.org.br/Act/production.psgi
+
+# --error-log /var/log/nginx/starman_yapcbrasil_error_log.log \
 
