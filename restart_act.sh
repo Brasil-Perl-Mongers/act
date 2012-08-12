d3f3b786b6a7abbba116445174594ac6a58eaba3
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2012-08-11 23:08:35 -0300

    adicionado perlbrew no restart_act.sh
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

--- restart_act.sh
+++ restart_act.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# perlbrew
+source /data/tools/perl5/etc/bashrc
+
+# matando processo na marreta
 /bin/fuser 8091/tcp -k
 
+# iniciando act
 ACTHOME=/data/sites/yapcbrasil.org.br/Act plackup -I /data/sites/yapcbrasil.org.br/Act/lib  -s Starman --daemonize --port 8091 --host 10.0.0.6 --workers 3 --user=www-data /data/sites/yapcbrasil.org.br/Act/production.psgi
 
 
