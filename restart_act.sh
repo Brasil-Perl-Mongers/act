a2a37728873b940d35dd25cb572ad9758bc47461
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2012-08-12 02:10:57 -0300

    starman error-log
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

--- restart_act.sh
+++ restart_act.sh
@@ -19,7 +19,7 @@ ACTHOME=/data/sites/yapcbrasil.org.br/Act \
     --host 10.0.0.6 \
     --workers 3 \
     --user=www-data \
+    --error-log /var/log/starman/yapcbrasil_error_log.log \
     /data/sites/yapcbrasil.org.br/Act/production.psgi
 
-# --error-log /var/log/nginx/starman_yapcbrasil_error_log.log \
 
